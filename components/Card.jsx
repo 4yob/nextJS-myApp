964fceeae5ac8fdfe86562c18552ceefcbd511e1
Alejandra Barros <alejandra.barros@aluno.senai.br>
2025-02-20 14:08:22 -0300

    refactor: adjust cards

--- components/Card.jsx
+++ components/Card.jsx
@@ -2,6 +2,10 @@ import styles from '../styles/Card.module.css';
 
 export default function Card() {
     return (
-        <div className={styles.card}></div>
+        <div className={styles.card}>
+            <h1 className={styles.title}>QUADROS ELEMENTOS DO MAR</h1>
+            <p className={styles.text}>COR: AZUL</p>
+            <p className={styles.text}>R$35.00</p>
+        </div>
     );
 }
