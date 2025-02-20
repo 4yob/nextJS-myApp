4c78c8c0045b74b4fe7deeb58203c2660cc5a0a9
Alejandra Barros <alejandra.barros@aluno.senai.br>
2025-02-20 13:46:13 -0300

    feat: create footer

---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Footer.module.css';
+
+export default function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <p className={styles.p}>Alejandra Barros</p>
+        </footer>
+    );
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import styles from '../styles/Home.module.css';
 import Header from '../components/Header';
 import Card from '../components/Card';
+import Footer from '../components/Footer';
 
 export default function Home() {
     return (
@@ -18,6 +19,7 @@ export default function Home() {
                 </aside>
             </section>
             </main>
+            <Footer />
         </div>
     );
 }
